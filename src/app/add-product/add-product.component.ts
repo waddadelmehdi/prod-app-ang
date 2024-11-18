368a831ad6069b170a2298251fd39a7d4f29d387
WADDAD EL MEHDI <waddadelmehdi@gmail.com>
2024-11-18 17:37:05 +0100

    commit

---
+++ src/app/add-product/add-product.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Product} from "../model/product.model";
+import {ProductService} from "../services/product.service";
+
+@Component({
+  selector: 'app-add-product',
+  templateUrl: './add-product.component.html',
+  styleUrl: './add-product.component.css'
+})
+export class AddProductComponent implements OnInit {
+
+  productForm!: FormGroup;
+
+
+  constructor(private fb: FormBuilder,private productService: ProductService) {
+  }
+
+  ngOnInit() {
+    this.productForm = this.fb.group({
+      name:this.fb.control("", [Validators.required]),
+      price:this.fb.control(0, [Validators.required]),
+      selected:this.fb.control(false, [Validators.required]),
+    });
+  }
+
+  saveProduct() {
+    let product : Product= {
+      id : 0,
+      name : this.productForm.value.name,
+      price : parseFloat(this.productForm.value.price),
+      selected : this.productForm.value.selected
+    }
+    this.productService.saveProduct(product)
+  }
+}
